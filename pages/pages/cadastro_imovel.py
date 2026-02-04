2a3b70e6a0379fb02792e0efe6b67baa88f0f310
tecflowsolucoess <tecflowsolucoess@gmail.com>
2026-02-04 00:41:49 -0300

    Update cadastro_imovel.py

--- pages/pages/cadastro_imovel.py
+++ pages/pages/cadastro_imovel.py
@@ -1,4 +1,10 @@
 import streamlit as st
+
+if not st.session_state.get("logado"):
+    st.warning("Você precisa estar logado para acessar esta página.")
+    st.stop()
+
+import streamlit as st
 from services.banco import salvar_imovel
 
 if not st.session_state.get("logado"):
