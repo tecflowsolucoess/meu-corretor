dda881009e532c1451e4ceb1776735642b684776
tecflowsolucoess <tecflowsolucoess@gmail.com>
2026-02-04 00:46:38 -0300

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
 if not st.session_state.get("logado"):
+    st.warning("Você precisa estar logado")
+    st.stop()
+
+import streamlit as st
+
+if not st.session_state.get("logado"):
     st.warning("Você precisa estar logado para acessar esta página.")
     st.stop()
 
